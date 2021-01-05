1fae2e9c35a456fdc44a567ac01873f652aafc19
Richardson Bruno <richardsonbrc@gmail.com>
2021-01-05 12:46:26 -0300

    configuração inicial

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import App from './App';
+
+export default function Routes () {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={App} />
+      </Switch>
+    </Router>
+  );
+}
